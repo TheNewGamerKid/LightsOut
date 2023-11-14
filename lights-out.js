afae391dc58d679f77378a3771b67fd4bb5b2c36
TheNewGamerKid <robert.ikerd@outlook.com>
2023-11-14 07:42:53 -0800

    Made PWA Compliant and Cached site

--- lights-out.js
+++ lights-out.js
@@ -1,101 +1,105 @@
 const shapes9 = [
   `111111111011100100000000000001000100001000100000010000010000010001111100000000000`,
-  `111111111011100100000000000001000100001000100000000000001111100010000010000000000`, 
-  `000000000000111000001111100011000110011011110011000110001111100000111000000000000`, 
-  `000000000000111000001000100010000010010000010010000010001000100000111000000000000`, 
-  `000000000000010000000010000000111000000111000001111100001111100001111100000000000`, 
+  `111111111011100100000000000001000100001000100000000000001111100010000010000000000`,
+  `000000000000111000001111100011000110011011110011000110001111100000111000000000000`,
+  `000000000000111000001000100010000010010000010010000010001000100000111000000000000`,
+  `000000000000010000000010000000111000000111000001111100001111100001111100000000000`,
   `000000000010000000011000000011100000011110000011111000011111100011111110000000000`,
   `000000000000000100000000010011000010011111100001111100001000100001000100000000000`,
-  `000000000000010000000010000000111000000111000001111100001111100000010000000010000`
-]
+  `000000000000010000000010000000111000000111000001111100001111100000010000000010000`,
+];
 const shapes7 = [
-  `0000000001010000101000000000010001000111000000000`, 
+  `0000000001010000101000000000010001000111000000000`,
   `0000000001010000101000000000001110001000100000000`,
   `0000000001110001000100100010010001000111000000000`,
   `0000000001110001111100111110011111000111000000000`,
   `0000000000100000010000011100001110001111100000000`,
   `0000000010000001100000111000011110001111100000000`,
   `0000000001110001111100001000000100000010000001000`,
-  `0000000001110001111100111110011111000010000001000`
-]
+  `0000000001110001111100111110011111000010000001000`,
+];
 const shapes5 = [
-  `0101000000100010111000000`, 
-  `0101000000011101000100000`, 
+  `0101000000100010111000000`,
+  `0101000000011101000100000`,
   `0000001110011100111000000`,
-  `0000001000011000111000000`, 
+  `0000001000011000111000000`,
   `0000001100111101111100000`,
-  `0010001110011100111000100`
-]
-let game = document.querySelector("#game")
-const homeBtn = document.querySelector("#title")
-let history = []
-const chance = .35
+  `0010001110011100111000100`,
+];
+let game = document.querySelector('#game');
+const homeBtn = document.querySelector('#title');
+let history = [];
+const chance = 0.35;
 
 const undoHandler = (size) => {
-  console.log(history)
-  if(history.length > 1) {
-    history.pop()
+  console.log(history);
+  if (history.length > 1) {
+    history.pop();
 
-    const gameBoard = document.querySelector(".game-board")
-    gameBoard.innerHTML = ''
-    const prevState = history[history.length - 1].split('')
-    let numLoops = 0
-    for(let i = 0; i < size; i++) {
+    const gameBoard = document.querySelector('.game-board');
+    gameBoard.innerHTML = '';
+    const prevState = history[history.length - 1].split('');
+    let numLoops = 0;
+    for (let i = 0; i < size; i++) {
       gameBoard.innerHTML += `
         <div id="row${i + 1}">
         </div>
-      `
-      const row = document.querySelector(`#row${i + 1}`)
-      for(let j = 0; j < size; j++) {
+      `;
+      const row = document.querySelector(`#row${i + 1}`);
+      for (let j = 0; j < size; j++) {
         row.innerHTML += `
-          <button id="column${j + 1} row${i + 1}" class="game-btn size-${size} ${prevState[numLoops] === "1" ? "on" : "off"}">
+          <button id="column${j + 1} row${
+          i + 1
+        }" class="game-btn size-${size} ${
+          prevState[numLoops] === '1' ? 'on' : 'off'
+        }">
           </button>
-        `
-        numLoops++
+        `;
+        numLoops++;
       }
     }
 
-    const btns = document.querySelectorAll(".game-btn")
-    for(let btn of btns) {
-      btn.addEventListener("click", updateGame)
+    const btns = document.querySelectorAll('.game-btn');
+    for (let btn of btns) {
+      btn.addEventListener('click', updateGame);
     }
   }
-}
+};
 
 const resetHandler = (size) => {
-  const gameBoard = document.querySelector(".game-board")
-  gameBoard.innerHTML = ''
-  const prevState = history[0].split('')
-  let numLoops = 0
-  for(let i = 0; i < size; i++) {
+  const gameBoard = document.querySelector('.game-board');
+  gameBoard.innerHTML = '';
+  const prevState = history[0].split('');
+  let numLoops = 0;
+  for (let i = 0; i < size; i++) {
     gameBoard.innerHTML += `
       <div id="row${i + 1}">
       </div>
-    `
-    const row = document.querySelector(`#row${i + 1}`)
-    for(let j = 0; j < size; j++) {
+    `;
+    const row = document.querySelector(`#row${i + 1}`);
+    for (let j = 0; j < size; j++) {
       row.innerHTML += `
-        <button id="column${j + 1} row${i + 1}" class="game-btn size-${size} ${prevState[numLoops] === "1" ? "on" : "off"}">
+        <button id="column${j + 1} row${i + 1}" class="game-btn size-${size} ${
+        prevState[numLoops] === '1' ? 'on' : 'off'
+      }">
         </button>
-      `
-      numLoops++
+      `;
+      numLoops++;
     }
   }
-  const btns = document.querySelectorAll(".game-btn")
-  let curGameState = ''
-  history = []
-  for(let btn of btns) {
-    curGameState += btn.classList[2] === "on" ? "1" : "0"
-    btn.addEventListener("click", updateGame)
+  const btns = document.querySelectorAll('.game-btn');
+  let curGameState = '';
+  history = [];
+  for (let btn of btns) {
+    curGameState += btn.classList[2] === 'on' ? '1' : '0';
+    btn.addEventListener('click', updateGame);
   }
 
-  history.push(curGameState)
-}
+  history.push(curGameState);
+};
 
 const renderGame = (size, gameMode) => {
-  let state = gameMode === "match"
-    ? "off"
-    : ""
+  let state = gameMode === 'match' ? 'off' : '';
 
   game.innerHTML = `
     <div class="game-board" id="game-board">
@@ -105,95 +109,109 @@ const renderGame = (size, gameMode) => {
       <button class="undo btn" id="undo">
         Undo
       </button>
-      ${gameMode === "match" ? `
+      ${
+        gameMode === 'match'
+          ? `
         <div id="match-container">
         </div>
-      `: ''}
+      `
+          : ''
+      }
       <button class="undo btn" id="reset">
         Reset
       </button>
     </div>
-  `
+  `;
 
-  const gameBoard = document.querySelector(".game-board")
-  for(let i = 0; i < size; i++) {
+  const gameBoard = document.querySelector('.game-board');
+  for (let i = 0; i < size; i++) {
     gameBoard.innerHTML += `
       <div id="row${i + 1}">
       </div>
-    `
-    const row = document.querySelector(`#row${i + 1}`)
-    for(let j = 0; j < size; j++) {
-      state = gameMode === "classic" ? Math.random() < chance ? "on" : "off" : "off"
+    `;
+    const row = document.querySelector(`#row${i + 1}`);
+    for (let j = 0; j < size; j++) {
+      state =
+        gameMode === 'classic'
+          ? Math.random() < chance
+            ? 'on'
+            : 'off'
+          : 'off';
       row.innerHTML += `
-        <button id="column${j + 1} row${i + 1}" class="game-btn size-${size} ${state}">
+        <button id="column${j + 1} row${
+        i + 1
+      }" class="game-btn size-${size} ${state}">
         </button>
-      `
+      `;
     }
   }
 
-  const undo = document.querySelector("#undo")
-  undo.addEventListener("click", () => {undoHandler(size)})
+  const undo = document.querySelector('#undo');
+  undo.addEventListener('click', () => {
+    undoHandler(size);
+  });
 
-  const reset = document.querySelector("#reset")
-  reset.addEventListener("click", () => {resetHandler(size)})
-}
+  const reset = document.querySelector('#reset');
+  reset.addEventListener('click', () => {
+    resetHandler(size);
+  });
+};
 
 const gameButtonHandler = (e) => {
-  let size = e.target.value
+  let size = e.target.value;
 
-  game.style.flexDirection = "column"
-  renderGame(size, "classic")
+  game.style.flexDirection = 'column';
+  renderGame(size, 'classic');
 
-  const btns = document.querySelectorAll(".game-btn")
-  let curGameState = ''
-  history = []
-  for(let btn of btns) {
-    curGameState += btn.classList[2] === "on" ? "1" : "0"
-    btn.addEventListener("click", updateGame)
+  const btns = document.querySelectorAll('.game-btn');
+  let curGameState = '';
+  history = [];
+  for (let btn of btns) {
+    curGameState += btn.classList[2] === 'on' ? '1' : '0';
+    btn.addEventListener('click', updateGame);
   }
 
-  history.push(curGameState)
-
-}
+  history.push(curGameState);
+};
 
 const toggleState = (col, row) => {
-  const btn = document.getElementById(`column${col} row${row}`)
-  if(btn !== null) {
-    const btnState = btn.classList[2]
-    btn.classList.remove(btnState)
-    btn.classList.add(btnState === "on" ? "off" : "on")
+  const btn = document.getElementById(`column${col} row${row}`);
+  if (btn !== null) {
+    const btnState = btn.classList[2];
+    btn.classList.remove(btnState);
+    btn.classList.add(btnState === 'on' ? 'off' : 'on');
   }
-}
+};
 
 const updateGame = (e) => {
-  let id = e.target.id
-  const row = parseInt(id.slice(id.indexOf(" ") + 4, id.length))
-  const column = parseInt(id.slice(6, id.indexOf(" ")))
+  let id = e.target.id;
+  const row = parseInt(id.slice(id.indexOf(' ') + 4, id.length));
+  const column = parseInt(id.slice(6, id.indexOf(' ')));
 
-  toggleState(column + 1, row)
-  toggleState(column, row + 1)
-  toggleState(column - 1, row)
-  toggleState(column, row - 1)
-  toggleState(column, row)
+  toggleState(column + 1, row);
+  toggleState(column, row + 1);
+  toggleState(column - 1, row);
+  toggleState(column, row - 1);
+  toggleState(column, row);
 
-  const gameBtns = document.querySelectorAll(".game-btn")
-  let curGameState = ''
-  for(let btn of gameBtns) {
-    curGameState += btn.classList[2] === "on" ? "1" : "0"
+  const gameBtns = document.querySelectorAll('.game-btn');
+  let curGameState = '';
+  for (let btn of gameBtns) {
+    curGameState += btn.classList[2] === 'on' ? '1' : '0';
   }
 
-  history.push(curGameState)
+  history.push(curGameState);
 
-  const matchContainer = document.querySelector("#match-container")
-  if(matchContainer === null) {
-    classicWinCheck()
+  const matchContainer = document.querySelector('#match-container');
+  if (matchContainer === null) {
+    classicWinCheck();
   } else {
-    matchWinCheck()
+    matchWinCheck();
   }
-}
+};
 
 const modeBtnHandler = (e) => {
-  game.style.flexDirection = "row"
+  game.style.flexDirection = 'row';
   game.innerHTML = `
     <button class="difficulty btn" value="5">
       5 x 5
@@ -204,20 +222,20 @@ const modeBtnHandler = (e) => {
     <button class="difficulty btn" value="9">
       9 x 9
     </button>
-  `
+  `;
 
-  if(e.target.value === "classic") {
-    const buttons = document.querySelectorAll(".difficulty")
-    for(let btn of buttons) {
-      btn.addEventListener("click", gameButtonHandler)
+  if (e.target.value === 'classic') {
+    const buttons = document.querySelectorAll('.difficulty');
+    for (let btn of buttons) {
+      btn.addEventListener('click', gameButtonHandler);
     }
   } else {
-    const buttons = document.querySelectorAll(".difficulty")
-    for(let btn of buttons) {
-      btn.addEventListener("click", matchBtnHandler)
+    const buttons = document.querySelectorAll('.difficulty');
+    for (let btn of buttons) {
+      btn.addEventListener('click', matchBtnHandler);
     }
   }
-}
+};
 
 const modeRender = () => {
   game.innerHTML = `
@@ -229,31 +247,31 @@ const modeRender = () => {
         Match
       </button>
     </div>
-  `
+  `;
 
-  const modeBtns = document.querySelectorAll(".mode")
-  for(let btn of modeBtns) {
-    btn.addEventListener("click", modeBtnHandler)
+  const modeBtns = document.querySelectorAll('.mode');
+  for (let btn of modeBtns) {
+    btn.addEventListener('click', modeBtnHandler);
   }
-}
+};
 
 const matchWinCheck = () => {
-  const gameBtns = document.querySelectorAll(".game-btn")
-  const matchBtns = document.querySelectorAll(".match-btn")
-  let winStatus = true
+  const gameBtns = document.querySelectorAll('.game-btn');
+  const matchBtns = document.querySelectorAll('.match-btn');
+  let winStatus = true;
 
-  for(let i = 0; i < gameBtns.length; i++) {
-    let matchClass = matchBtns[i].classList[2]
-    let gameClass = gameBtns[i].classList[2]
-    if(matchClass === gameClass) {
+  for (let i = 0; i < gameBtns.length; i++) {
+    let matchClass = matchBtns[i].classList[2];
+    let gameClass = gameBtns[i].classList[2];
+    if (matchClass === gameClass) {
     } else {
-      winStatus = false
-      break
+      winStatus = false;
+      break;
     }
   }
-  
-  if(winStatus === true) {
-    const body = document.querySelector('body')
+
+  if (winStatus === true) {
+    const body = document.querySelector('body');
     body.innerHTML = `
       <div id="title">
         You Win!
@@ -262,10 +280,10 @@ const matchWinCheck = () => {
         Restart
       </button>
       <script src="lights-out.js"></script>
-    `
+    `;
 
-    const restartBtn = document.querySelector("#restart")
-    restartBtn.addEventListener("click", () => {
+    const restartBtn = document.querySelector('#restart');
+    restartBtn.addEventListener('click', () => {
       body.innerHTML = `
         <button id="title">
           Lights Out
@@ -274,30 +292,30 @@ const matchWinCheck = () => {
 
         </div>
         <script src="lights-out.js"></script>
-      `
+      `;
 
-      history = []
-      console.log(history)
-      game = document.querySelector("#game")
+      history = [];
+      console.log(history);
+      game = document.querySelector('#game');
 
-      modeRender()
-    })
+      modeRender();
+    });
   }
-}
+};
 
 const classicWinCheck = () => {
-  const gameBtns = document.querySelectorAll(".game-btn")
-  let winStatus = true
-  for(let btn of gameBtns) {
-    let state = btn.classList[2]
-    if(state === "on") {
-      winStatus = false
-      break
+  const gameBtns = document.querySelectorAll('.game-btn');
+  let winStatus = true;
+  for (let btn of gameBtns) {
+    let state = btn.classList[2];
+    if (state === 'on') {
+      winStatus = false;
+      break;
     }
   }
 
-  if(winStatus === true) {
-    const body = document.querySelector('body')
+  if (winStatus === true) {
+    const body = document.querySelector('body');
     body.innerHTML = `
       <div id="title">
         You Win!
@@ -306,10 +324,10 @@ const classicWinCheck = () => {
         Restart
       </button>
       <script src="lights-out.js"></script>
-    `
+    `;
 
-    const restartBtn = document.querySelector("#restart")
-    restartBtn.addEventListener("click", () => {
+    const restartBtn = document.querySelector('#restart');
+    restartBtn.addEventListener('click', () => {
       body.innerHTML = `
         <button id="title">
           Lights Out
@@ -318,58 +336,63 @@ const classicWinCheck = () => {
 
         </div>
         <script src="lights-out.js"></script>
-      `
+      `;
 
-      game = document.querySelector("#game")
+      game = document.querySelector('#game');
 
-      modeRender()
-    })
+      modeRender();
+    });
   }
-}
+};
 
-homeBtn.addEventListener("click", modeRender)
+homeBtn.addEventListener('click', modeRender);
 
-modeRender()
+modeRender();
 
 const matchBtnHandler = (e) => {
   let numLoops = 0;
   let size = e.target.value;
-  const shape = e.target.value == 5 
-    ? shapes5[Math.floor(Math.random() * shapes5.length)] 
-    : e.target.value == 7 
-      ? shapes7[Math.floor(Math.random() * shapes7.length)] 
-      : shapes9[Math.floor(Math.random() * shapes9.length)]
-  
-  game.style.flexDirection = "column"
-  renderGame(size, "match")
-  const matchCntr = document.querySelector("#match-container")
+  const shape =
+    e.target.value == 5
+      ? shapes5[Math.floor(Math.random() * shapes5.length)]
+      : e.target.value == 7
+      ? shapes7[Math.floor(Math.random() * shapes7.length)]
+      : shapes9[Math.floor(Math.random() * shapes9.length)];
+
+  game.style.flexDirection = 'column';
+  renderGame(size, 'match');
+  const matchCntr = document.querySelector('#match-container');
   matchCntr.innerHTML = `
     <div class="game-board" id="match-board">
     </div>
-  `
-  const shapeArr = shape.split('')
-  const matchBoard = document.querySelector("#match-board")
-  for(let i = 0; i < size; i++) {
+  `;
+  const shapeArr = shape.split('');
+  const matchBoard = document.querySelector('#match-board');
+  for (let i = 0; i < size; i++) {
     matchBoard.innerHTML += `
       <div id="match-row${i + 1}" style="display: flex">
       </div>
-    `
-    const row = document.querySelector(`#match-row${i + 1}`)
-    for(let j = 0; j < size; j++) {
+    `;
+    const row = document.querySelector(`#match-row${i + 1}`);
+    for (let j = 0; j < size; j++) {
       row.innerHTML += `
-        <div id="match column${j + 1} row${i + 1}" class="match-btn match-size-${size} ${shapeArr[numLoops] === "1" ? "on" : "off"}">
+        <div id="match column${j + 1} row${
+        i + 1
+      }" class="match-btn match-size-${size} ${
+        shapeArr[numLoops] === '1' ? 'on' : 'off'
+      }">
         </div>
-      `
-      numLoops++
+      `;
+      numLoops++;
     }
   }
-  const btns = document.querySelectorAll(".game-btn")
-  let curGameState = ''
-  history = []
-  for(let btn of btns) {
-    curGameState += btn.classList[2] === "on" ? "1" : "0"
-    btn.addEventListener("click", updateGame)
+  const btns = document.querySelectorAll('.game-btn');
+  let curGameState = '';
+  history = [];
+  for (let btn of btns) {
+    curGameState += btn.classList[2] === 'on' ? '1' : '0';
+    btn.addEventListener('click', updateGame);
   }
 
-  history.push(curGameState)
-}
+  history.push(curGameState);
+};
---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_VERSION = 'v1';
+const STATIC_ASSESTS = [
+  'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,600;1,400&display=swap',
+  '/sw-registration.js',
+  '/lights-out.css',
+  '/lights-out.js',
+  '/manifest.json',
+  'index.html',
+  '/icon.jpg',
+  '/icon.ico',
+  '/',
+];
+
+self.addEventListener('install', (event) => {
+  console.log(STATIC_ASSESTS);
+  event.waitUntil(
+    caches.open(`static-site-${CACHE_VERSION}`).then((cache) => {
+      cache.addAll(STATIC_ASSESTS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    cache.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => !key.includes(CACHE_VERSION))
+      ).map((key) => caches.delete(key));
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
